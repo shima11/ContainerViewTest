bcd9fb83e21b093dcc4cf913022cb01164fd9efe
shima <shima11@users.noreply.github.com>
2016-11-26 01:30:23 +0900

    ContainerViewだけドラッグ判定（PanGesture）

--- ContainerViewTest/ViewController.swift
+++ ContainerViewTest/ViewController.swift
@@ -15,28 +15,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.lightGray
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.panGesture(sender:)))
+        containerView.addGestureRecognizer(panGesture)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func panGesture(sender: UIPanGestureRecognizer) {
+        print("pan gesture")
     }
-
 
 }
 
-extension ViewController {
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+//extension ViewController {
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        super.touchesBegan(touches, with: event)
-//        let touchEvent = touches.first
-        print("touch began")
-    }
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touch ended")
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touch moved")
-    }
-}
+////        let touchEvent = touches.first
+//        print("touch began")
+//    }
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesEnded(touches, with: event)
+//        print("touch ended")
+//    }
+//    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesMoved(touches, with: event)
+//        print("touch moved")
+////        let touchEvent = touches.first
+////        if (touchEvent?.view?.isEqual(containerView))! {
+////            print("container view moved")
+////        }
+//    }
+//}
 
