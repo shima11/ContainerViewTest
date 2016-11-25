fc54eece4f7a770cd902f9e9b5728469f6eede65
shima <shima11@users.noreply.github.com>
2016-11-25 17:39:37 +0900

    Initial Commit

---
+++ ContainerViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainerViewTest
+//
+//  Created by shima jinsei on 2016/11/25.
+//  Copyright © 2016年 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
