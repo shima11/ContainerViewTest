a56d0836e42c364797dd33b97078bce28e57e4f4
shima <shima11@users.noreply.github.com>
2016-11-25 23:52:45 +0900

    ContainerViewの配置、画面タッチ判定

--- ContainerViewTest/ViewController.swift
+++ ContainerViewTest/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,20 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesBegan(touches, with: event)
+//        let touchEvent = touches.first
+        print("touch began")
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touch ended")
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touch moved")
+    }
 }
 
