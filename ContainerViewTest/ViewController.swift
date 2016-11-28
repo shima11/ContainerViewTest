a1aa47cfb0d3eed329b20968c1f1d901a8ed4902
shima <shima11@users.noreply.github.com>
2016-11-28 22:50:46 +0900

    scrollviewで工夫したバージョン

--- ContainerViewTest/ViewController.swift
+++ ContainerViewTest/ViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
+enum ScrollState {
+    case Bottom
+    case Medium
+    case Top
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var containerViewHeight: NSLayoutConstraint!
+    
+    
+    var state: ScrollState = ScrollState.Top
     
     var currentPoint: CGPoint!
     var prePoint: CGPoint!
@@ -24,10 +36,26 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = UIColor.lightGray
         
+        let edgeInsets = UIEdgeInsets(top: containerView.frame.height - 120, left: 0, bottom: 0, right: 0)
+        scrollView.contentInset = edgeInsets
+        
+        scrollView.delegate = self
+        
+        
         animator = UIDynamicAnimator(referenceView: self.view)
         
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.panGesture(sender:)))
-        containerView.addGestureRecognizer(panGesture)
+//        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.panGesture(sender:)))
+//        containerView.addGestureRecognizer(panGesture)
+        
+        
+//        containerView.frame = CGRect(x:containerView.frame.minX, y: containerView.frame.minY, width: containerView.frame.width, height: self.view.frame.height*0.8)
+        
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     func panGesture(sender: UIPanGestureRecognizer) {
@@ -36,8 +64,9 @@ class ViewController: UIViewController {
         }
         if sender.state == .changed {
             currentPoint = sender.location(in: self.view)
-            containerView.center.y += currentPoint.y - prePoint.y
-
+            let distance = currentPoint.y - prePoint.y
+            containerView.frame = CGRect(x:containerView.frame.minX, y: containerView.frame.minY+distance, width: containerView.frame.width, height: self.view.frame.height*0.8)
+            
 //            if containerView.center.y-containerView.frame.height/2 < self.view.frame.height*0.02 {
 //                containerView.center.y -= currentPoint.y - prePoint.y
 //            }
@@ -46,6 +75,9 @@ class ViewController: UIViewController {
 
         }
         if sender.state == .ended {
+            let viewcontroller = self.childViewControllers[0]
+            print(viewcontroller.view.frame)
+
 //            if currentPoint.y < self.view.frame.height*0.4 {
 //                print("0.4")
 //                snapAnimation(point: CGPoint(x: self.view.frame.width/2, y: self.view.frame.height*0.2))
@@ -71,6 +103,14 @@ class ViewController: UIViewController {
 }
 
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
+}
+
+
+// touch event
 //extension ViewController {
 //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        super.touchesBegan(touches, with: event)
